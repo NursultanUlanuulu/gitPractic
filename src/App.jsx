622f886169a2083b1d8819be9ecf449493db7cc4
Nursultan <144236584+NursultanUlanuulu@users.noreply.github.com>
2025-07-15 14:47:06 +0600

    Revert "UI/card"

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-
-
-
-import Header from "./components/Header/Header"
-
-import Cards from "./components/Cards"
 function App() {
 	return (
 		<>
 			<h1>{/* Собирайте компоненты здесь */}</h1>
-
-			<Header/>
-			<Cards />
-
-
 		</>
 	)
 }
